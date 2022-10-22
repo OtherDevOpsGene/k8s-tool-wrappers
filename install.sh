1359a0090c740e4b29435f048e45325c4aa62bb2
Gene Gotimer <eugene.gotimer@steampunk.com>
2022-10-22 17:33:38 -0400

    Fixed grype, syft, trivy

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ dest="/usr/local/bin/"
 # switch to script directory
 cd "$(dirname -- ${0})" || exit
 
-for script in checkov eksctl grype syft
+for script in checkov eksctl grype syft trivy
 do
   install -v "${script}" "${dest}"
 done
